2a93a9f37062fbe6c2ac7538594cd15a4119197c
Apple <abulbasharsaurov00@gmail.com>
2025-05-05 11:21:00 +0600

    e

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Php Info </h1>
+    <form action="process.php" method="post">
+        <input type="submit" value="Show PHP Info">
+</body>
+</html>
